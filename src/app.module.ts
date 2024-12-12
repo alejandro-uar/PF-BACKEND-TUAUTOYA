39d7d59a808517affbb9fdbb7e6b647ca18f3072
Alejandro Luna <alejandro2002.uar@gmail.com>
2024-12-11 22:58:01 -0300

    Add: Config of typeOrm & packege.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeormConfig from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [typeormConfig] }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('typeorm'),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,17 @@
+// config/typeorm.config.ts
+import { registerAs } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+
+export default registerAs('typeorm', () => ({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  dropSchema: false,
+  logging: false,
+  synchronize: true
+}));
+
